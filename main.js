78074524fcff32f23c9df9c4159cdb90d26b20ac
Diego <diegoburgo2005@gmail.com>
2025-02-20 15:50:20 +0100

    deel op school

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import {
 } from './lib.js';
 
 runTitle();
+warmup2();
 
 function warmup1() {
     console.log("Exercise week 1");
@@ -12,8 +13,38 @@ function warmup1() {
 
 function warmup2() {
     console.log("Exercise week 2");
-    const scores = getRandomScores(10);
+    let scores = getRandomScores(10);
+    const button2a = document.querySelector('#button-2a');
+    button2a.addEventListener ('click', function (){
+        console.log ('button1 clicked');
+        let html = '<ul>';
+        scores.forEach(function(score) {
+            html += ` <li>${score}</li>` ;
+        });
+        html += '</ul>';
+        document.querySelector('#content-2').innerHTML = html;
+    });
+    const button2b = document.querySelector('#button-2b');
+    button2b.addEventListener ('click', function (){
+        console.log ('button2 clicked');
+        scores = scores.sort(function(a, b) {
+            if (a > b) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+
+        const sum = scores.reduce(function (total, current) {
+            return total + current;
+        });
+    });
+
+
+    
 }
+
+
 
 function warmup3() {
     let student = getStudentJSON();
